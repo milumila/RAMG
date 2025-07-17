d5d098f05bd6e8104a967f5f0e1edc67de0eb367
milumila <milanpanda39@gmail.com>
2025-07-17 21:36:31 +0530

    Update saini.py

--- saini.py
+++ saini.py
@@ -258,7 +258,7 @@ async def download_video(url,cmd, name):
 
 
 async def send_doc(bot: Client, m: Message, cc, ka, cc1, prog, count, name):
-    reply = await m.reply_text(f"**★彡 Uploading 彡★ ...⏳**\n\n📚𝐓𝐢𝐭𝐥𝐞 » {name}\n\n✦𝐁𝐨𝐭 𝐌𝐚𝐝𝐞 𝐁𝐲 ✦ 𝙎𝘼𝙄𝙉𝙄 𝘽𝙊𝙏𝙎🐦")
+    reply = await m.reply_text(f"**★彡 Uploading 彡★ ...⏳**\n\n📚𝐓𝐢𝐭𝐥𝐞 » {name}\n\n✦𝐁𝐨𝐭 𝐌𝐚𝐝𝐞 𝐁𝐲 ✦ 🅼🅰🅷🅰🅳🅴🆅🐦")
     time.sleep(1)
     start_time = time.time()
     await bot.send_document(ka, caption=cc1)

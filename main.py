633a656f5775e3d21c1b7b12dddce63ded50c052
milumila <milanpanda39@gmail.com>
2025-07-17 21:39:44 +0530

    Update main.py

--- main.py
+++ main.py
@@ -70,9 +70,9 @@ photozip = 'https://envs.sh/cD_.jpg'
 
 
 # Inline keyboard for start command
-BUTTONSCONTACT = InlineKeyboardMarkup([[InlineKeyboardButton(text="📞 Contact", url="https://t.me/saini_contact_bot")]])
+BUTTONSCONTACT = InlineKeyboardMarkup([[InlineKeyboardButton(text="📞 Contact", url="https://t.me/t.me/@Kar_000000")]])
 keyboard = InlineKeyboardMarkup([
-        [InlineKeyboardButton(text="🛠️ Help", url="https://t.me/+3k-1zcJxINYwNGZl"), InlineKeyboardButton(text="🛠️ Repo", url="https://github.com/cyberseller999/saini-txt-direct")],
+        [InlineKeyboardButton(text="🛠️ Help", url="https://t.me/t.me/@Kar_000000"), InlineKeyboardButton(text="🛠️ Repo", url="https://github.com/")],
 ])
 
 # Image URLs for the random image feature
